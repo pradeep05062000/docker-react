fcaf61c066b8460f43f334f3bc97979c5fb92d3c
pradeep05062000 <inglepradeep05062000>
2021-06-21 06:39:51 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from stepen sir
+        </a>
+	<p>my name is pradeep</p>
+	<h1> RAKUL </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
